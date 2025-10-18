45c095b8123668f1ecf4bba53e86140b3616132a
beata-teck <batatefera7@gmail.com>
2025-10-18 10:30:10 -0800

    header skeleton initiation

---
+++ study-group-finder/src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>Study Group & Event Finder</h1>
+      <p>Connect. Learn. Grow together.</p>
+    </header>
+  );
+}
+
+export default Header;
